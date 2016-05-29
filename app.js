1993c38a88dbe3c4abd461db52139c2eb97723ce
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-05-29 11:35:32 +0200

    Progress

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ d3.csv('History of Famous People.csv', function (error, data) {
     if (error) {
         console.error(error);
     } else {
-        console.log(data);
-        var filtered = data.filter(d => d['Dead or Alive'] === 'Dead');
+        // console.log(data);
+        var filtered = data.filter(d => d['Dead or Alive'] === 'Dead')
+            .sort((a, b) => a.Birthyear - b.Birthyear);
         var chart = d3.select('#container')
+            .attr('height', filtered.length * 30)
             .append('g')
             .classed('chart', true);
         var scale = d3.time.scale()
             .range([120, 800])
-            .domain([-1000, 3000]);
+            .domain([-3500, 2000]);
         var persons = chart.selectAll('g.person')
             .data(filtered)
             .enter()
@@ -21,14 +23,15 @@ d3.csv('History of Famous People.csv', function (error, data) {
             .attr({
                 'y': 10
             })
-            .text(d => d.Name);
+            .text(d => d.Name + '(' + d.Birthyear + '-' + d.To + ')');
         persons
             .append('rect')
             .attr({
                 'x': d => scale(d.Birthyear),
-                width: d => scale(d.To - d.Birthyear),
-                height:30
+                'y':3,
+                width: d => scale(d.To)-scale(d.Birthyear),
+                height: 24
             })
-            .style('fill','beige');
+            .style('fill', 'beige');
     }
 });
