9f049559db2dc52e47cc58694e1d0808ff4471c7
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-06-01 12:46:14 +0200

    Map refactoring to class

--- src/map.ts
+++ src/map.ts
@@ -14,18 +14,17 @@ module app {
                 .append('g')
                 .classed('map', true);
             title(this._container, width, 'Number of famous people by country');
-
-
             var countries = this._container
                 .append('g')
                 .classed('countries', true)
                 .attr('transform', 'translate(0,50)');
-            this.initMap(countries, width, height);
-            var extent = [0, 1600];//d3.extent(nested, n => n.values.length);
+            var extent: [number, number] = [0, 1600];//d3.extent(nested, n => n.values.length);
             this._color = d3.scale.linear()
                 .domain(extent)
                 .interpolate(d3.interpolateHcl)
                 .range([d3.rgb('#C2E7D9'), d3.rgb('#0D0221')]);
+            this.initMap(countries, width, height);
+            this.initLegend(this._container, width, height, extent);
 
         }
 
@@ -58,6 +57,45 @@ module app {
             });
         }
 
+        private initLegend(container: d3.Selection<any>, width: number, height: number, extent: [number, number]) {
+            var legend = container.append('g')
+                .classed('legend', true)
+                .attr('transform', `translate(10,${height - 40})`);
+            var step = 100;
+            var range = d3.range(extent[0], extent[1] + step, step);
+            legend
+                .append('g')
+                .selectAll('rect')
+                .data(range)
+                .enter()
+                .append('rect')
+                .attr({
+                    'x': (d, i) => i * 10,
+                    'y': 0,
+                    'width': 10,
+                    'height': 10
+                })
+                .style({
+                    'fill': d => this._color(d)
+                });
+            var legnedText = legend.append('g')
+                .attr('transform', 'translate(0,25)');
+            legnedText.append('text')
+                .style({
+                    'text-anchor': 'middle'
+                })
+                .text(extent[0]);
+
+            legnedText.append('text')
+                .attr({
+                    'x': range.length * 10
+                })
+                .style({
+                    'text-anchor': 'middle'
+                })
+                .text(extent[1]);
+        }
+
         update(data: IHistory[]) {
             var key = 'Country Name';
             var nested = d3.nest()
@@ -77,96 +115,3 @@ module app {
         }
     }
 }
-
-
-
-function map(container, width, height, data) {
-    var map = d3.select(`#${container}`)
-        .attr({
-            width: width,
-            height: height
-        })
-        .append('g')
-        .classed('map', true);
-    //.attr('transform', 'translate(0,50)');
-    map.append('g')
-        .classed('title', true)
-        .attr('transform', `translate(${width / 2},30)`)
-        .append('text')
-        .text('Number of famous people by country');
-    var key = 'Country Name';
-    var nested = d3.nest()
-        .key(d => d[key])
-        .entries(data.filter(d => d[key] && d[key] !== "Unknown"));
-    d3.json('data/world.json', function (error, geo) {
-        if (error) {
-            console.error(error);
-        } else {
-            var countries = map.append('g')
-                .classed('countries', true)
-                .attr('transform', 'translate(0,50)');
-
-            // var extent = d3.extent(nested, n => n.values.length);
-            var extent = [0, 1600];//d3.extent(nested, n => n.values.length);
-            var color = d3.scale.linear()
-                .domain(extent)
-                .interpolate(d3.interpolateHcl)
-                .range([d3.rgb('#C2E7D9'), d3.rgb('#0D0221')]);
-            var projection = d3.geo.mercator()
-                .translate([width / 2, height / 2])
-                .scale(height / 6);
-            var pathGenerator = d3.geo.path().projection(projection);
-            countries.selectAll('path')
-                .data(geo.features)
-                .enter()
-                .append('path')
-                .attr('d', pathGenerator)
-                .style('fill', (d, i) => {
-                    var lowerCase = d.properties.name.toLowerCase();
-                    var index = nested.find(el => el.key.toLowerCase() === lowerCase);
-                    var value = index ? index.values.length : 0;
-                    var c = color(value);
-                    return c;
-                });
-
-            var legend = map.append('g')
-                .classed('legend', true)
-                .attr('transform', `translate(10,${height - 40})`);
-            var step = 100;
-            var range = d3.range(extent[0], extent[1] + step, step);
-            legend
-                .append('g')
-                .selectAll('rect')
-                .data(range)
-                .enter()
-                .append('rect')
-                .attr({
-                    'x': (d, i) => i * 10,
-                    'y': 0,
-                    'width': 10,
-                    'height': 10
-                })
-                .style({
-                    'fill': d => color(d)
-                });
-            var legnedText = legend.append('g')
-                .attr('transform', 'translate(0,25)');
-            legnedText.append('text')
-                .style({
-                    'text-anchor': 'middle'
-                })
-                .text(extent[0]);
-
-            legnedText.append('text')
-                .attr({
-                    'x': range.length * 10
-                })
-                .style({
-                    'text-anchor': 'middle'
-                })
-                .text(extent[1]);
-        }
-    });
-}
-//     });
-// }
