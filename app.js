a3868526743c188d4178952af1b61bb6031e2c0a
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-06-07 21:21:02 +0200

    Basic axis

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 (function () {
+    var width = 800;
+    var height = 600;
+    marginBottom = 50;
+    marginTop = 0;
+    marginLeft = 50;
+    marginRight = 50;
     'use strict';
-    d3.select('#container')
-        .append('rect')
-        .attr({
-            'x': 10,
-            'y': 10,
-            'width': 100,
-            'height': 100
-        });
+    var chartGroup = d3.select('svg')
+        .append('g')
+        .attr('transform', `translate(${marginLeft},${marginTop})`);
     d3.csv('Facebook Energy Sources.csv',
         function (d) {
             return {
                 year: +d.Year,
                 energySource: d['Energy Source'],
-                amount: +(d.Amount.replace('%',''))/100
+                amount: +(d.Amount.replace('%', '')) / 100
             }
         }, function (error, data) {
-        if (error) {
-            console.error(error);
-        } else {
-            var test = d3.nest()
-                .key((d) => d.year)
-                .entries(data);
-            console.log(test);
-            var keys = d3.keys(test);
-            console.log(keys);
-        }
-    })
+            if (error) {
+                console.error(error);
+            } else {
+                var test = d3.nest()
+                    .key((d) => d.year)
+                    .entries(data);
+                console.log(test);
+                var x0 = d3.scale.ordinal()
+                    .domain(test.map(d=>d.key))
+                    .rangeRoundBands([0, width - marginLeft - marginRight], 0.1);
+
+                var xAxis = d3.svg.axis().scale(x0);
+                var xAxisGroup = chartGroup
+                    .append('g')
+                    .attr('transform', `translate(0,${height - 50})`);
+                xAxisGroup.call(xAxis);
+            }
+        })
 } ());
