708eca101c0c89b2cfbb6603e5d147b8bb0ad9c9
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-06-18 09:24:31 +0200

    Displayed charts

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 (function () {
+    'use strict';
+    var chart = d3.chart(d3.select('#container'), 800, 600);
+    var chart2015 = d3.chart(d3.select('#container2015'), 800, 600);
+
     d3.csv('Female Corporate Talent Pipeline.csv',
         function (d) {
             return {
@@ -11,8 +15,37 @@
             if (error) {
                 console.error(error);
             } else {
-                console.log(data);
+                var byYear = d3.nest()
+                    .key(d => d.year)
+                    .entries(data);
+                chart.update(byYear[0].values);
+                chart2015.update(byYear[1].values);
+                //                 xScale.domain(byYear[0].values.map(d => d.level));
+                //                 xAxisGroup.call(xAxis);
 
+                //                 var seriesGroup = plotGroup.append('g')
+                //                     .classed('series', true);
+                //                 var items = seriesGroup.selectAll('.item')
+                //                     .data(byYear[0].values)
+                //                     .enter()
+                //                     .append('g')
+                //                     .classed('item', true);
+                //                 items.append('rect')
+                //                     .style('fill', 'pink')
+                //                     .attr({
+                //                         'x': d => xScale(d.level),
+                //                         'y': d => yScale(d.female),
+                //                         'width': xScale.rangeBand(),
+                //                         height: d => plotHeight - yScale(d.female)
+                //                     })
+                //                 items.append('rect')
+                //                     .style('fill', 'lightblue')
+                //                     .attr({
+                //                         'x': d => xScale(d.level),
+                //                         'y': 0,
+                //                         'width': xScale.rangeBand(),
+                //                         height: d => yScale(d.female)
+                //                     })
             }
         })
 } ());
---
+++ chart.js
@@ -0,0 +1,124 @@
+(function () {
+    'use strict';
+    d3.chart = function (container, width, height) {
+        // var width = 800;
+        // var height = 600;
+        var marginBottom = 50;
+        var marginTop = 20;
+        var marginLeft = 50;
+        var marginRight = 50;
+        var plotMargin = {
+            top: 50,
+            left: 0,
+            right: 0,
+            bottom: 0
+        };
+        var chartGroup = container
+            .append('g')
+            .attr('transform', `translate(${marginLeft},${marginTop})`);
+        var chartWidth = width - marginLeft - marginRight;
+        var chartHeight = height - marginTop - marginBottom;
+        var plotGroup = chartGroup
+            .append('g')
+            .attr('transform', `translate(${plotMargin.left},${plotMargin.top})`);
+        var plotWidth = chartWidth - plotMargin.left - plotMargin.right;
+        var plotHeight = chartHeight - plotMargin.top - plotMargin.bottom;
+
+        var xAxisGroup = plotGroup.append('g')
+            .classed('axis', true)
+            .attr('transform', `translate(${0},${plotHeight})`);
+        var xScale = d3.scale.ordinal()
+            .rangeBands([0, plotWidth]);
+        var xAxis = d3.svg.axis()
+            .orient('bottom')
+            .scale(xScale);
+
+        var yScale = d3.scale.linear()
+            .domain([0, 1])
+            .range([plotHeight, 0]);
+        var yAxis = d3.svg.axis()
+            .orient('left')
+            .tickFormat(d3.format('%'))
+            .scale(yScale);
+        var yAxisGroup = plotGroup.append('g')
+            .classed('axis', true)
+            .call(yAxis);
+        return {
+            update: function (data) {
+                //  var byYear = d3.nest()
+                //                 .key(d => d.year)
+                //                 .entries(data);
+                xScale.domain(data.map(d => d.level));
+                xAxisGroup.call(xAxis);
+
+                var seriesGroup = plotGroup.append('g')
+                    .classed('series', true);
+                var items = seriesGroup.selectAll('.item')
+                    .data(data)
+                    .enter()
+                    .append('g')
+                    .classed('item', true);
+                items.append('rect')
+                    .style('fill', 'pink')
+                    .attr({
+                        'x': d => xScale(d.level),
+                        'y': d => yScale(d.female),
+                        'width': xScale.rangeBand(),
+                        height: d => plotHeight - yScale(d.female)
+                    })
+                items.append('rect')
+                    .style('fill', 'lightblue')
+                    .attr({
+                        'x': d => xScale(d.level),
+                        'y': 0,
+                        'width': xScale.rangeBand(),
+                        height: d => yScale(d.female)
+                    })
+            }
+        }
+    }
+    // d3.csv('Female Corporate Talent Pipeline.csv',
+    //     function (d) {
+    //         return {
+    //             year: +d.Year,
+    //             level: d.Level,
+    //             female: +d.Female.replace('%', '') / 100,
+    //             male: +d.Male.replace('%', '') / 100
+    //         }
+    //     }, function (error, data) {
+    //         if (error) {
+    //             console.error(error);
+    //         } else {
+    //             var byYear = d3.nest()
+    //                 .key(d => d.year)
+    //                 .entries(data);
+    //             xScale.domain(byYear[0].values.map(d => d.level));
+    //             xAxisGroup.call(xAxis);
+
+    //             var seriesGroup = plotGroup.append('g')
+    //                 .classed('series', true);
+    //             var items = seriesGroup.selectAll('.item')
+    //                 .data(byYear[0].values)
+    //                 .enter()
+    //                 .append('g')
+    //                 .classed('item', true);
+    //             items.append('rect')
+    //                 .style('fill', 'pink')
+    //                 .attr({
+    //                     'x': d => xScale(d.level),
+    //                     'y': d => yScale(d.female),
+    //                     'width': xScale.rangeBand(),
+    //                     height: d => plotHeight - yScale(d.female)
+    //                 })
+    //             items.append('rect')
+    //                 .style('fill', 'lightblue')
+    //                 .attr({
+    //                     'x': d => xScale(d.level),
+    //                     'y': 0,
+    //                     'width': xScale.rangeBand(),
+    //                     height: d => yScale(d.female)
+    //                 })
+    //         }
+    //     })
+
+} ());
