ddb75490b178ae47dbcbde254d8f57761d1abeeb
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-06-20 21:31:30 +0200

    inital commit

---
+++ app.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+    var chart = d3.chart(d3.select('#container'), 800, 600);
+
+    d3.csv('Theft in Japan.csv',
+        function (d) {
+            return {
+                type: d['Theft Type'],
+                category: d['Category'],
+                percentage: +d['% of Total'].replace('%', '') / 100,
+               crimes:+d['Crimes']
+            }
+        }, function (error, data) {
+            if (error) {
+                console.error(error);
+            } else {
+                console.log(data);
+            }
+        })
+} ());
