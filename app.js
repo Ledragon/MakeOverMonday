c75884774eae413c135c187d6d83b7308549af94
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-06-26 19:08:51 +0200

    Number of submissions per week

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 (function () {
     'use strict';
+    var width = 800;
+    var height = 600;
+
+    var marginBottom = 50;
+    var marginTop = 50;
+    var marginLeft = 50;
+    var marginRight = 50;
+    var plotMargin = {
+        top: 40,
+        left: 0,
+        right: 0,
+        bottom: 0
+    };
+    var chartGroup = d3.select('svg')
+        .append('g')
+        .attr('transform', `translate(${marginLeft},${marginTop})`);
+    var chartWidth = width - marginLeft - marginRight;
+    var chartHeight = height - marginTop - marginBottom;
+
+    var plotGroup = chartGroup
+        .append('g')
+        .attr('transform', `translate(${plotMargin.left},${plotMargin.top})`);
+    var plotWidth = chartWidth - plotMargin.left - plotMargin.right;
+    var plotHeight = chartHeight - plotMargin.top - plotMargin.bottom;
+
+
+    chartGroup.append('g')
+        .classed('title', true)
+        .attr('transform', `translate(${chartWidth / 2},${0})`)
+        .append('text')
+        .text('Number of submissions per week');
+
+
+
+
+
+
+
 
     d3.csv('data/Makeover Monday.csv',
         (d) => {
@@ -11,16 +49,56 @@
                 originalLink: d['Original Link'],
                 name: d['Name'],
                 week: +d.Week,
-                datePinned: d['Date Pinned']
+                datePinned: moment(d['Date Pinned'], 'DD-MM-YY HH:mm')
             }
         },
         (error, data) => {
             if (error) {
                 console.error(error);
             } else {
-                console.log(data);
-                d3.nest()
-                    .key(d => d.Week);
+                var byWeek = d3.nest()
+                    .key(d => d.week)
+                    .entries(data);
+
+                var xScale = d3.scale.linear()
+                    .domain([0, d3.max(byWeek, w => +w.key)])
+                    .range([0, plotWidth]);
+                var xAxis = d3.svg.axis()
+                    .orient('bottom')
+                    .scale(xScale);
+                var xAxisGroup = plotGroup.append('g')
+                    .classed('axis', true)
+                    .attr('transform', `translate(${0},${plotHeight})`)
+                    .call(xAxis);
+
+
+                var yScale = d3.scale.linear()
+                    .domain([0, d3.max(byWeek, w => +w.values.length)])
+                    .range([plotHeight, 0]);
+                var yAxis = d3.svg.axis()
+                    .orient('left')
+                    .scale(yScale);
+                var yAxisGroup = plotGroup.append('g')
+                    .classed('axis', true)
+                    .call(yAxis);
+
+
+                var dataBound = plotGroup.selectAll('.data')
+                    .data(byWeek);
+                dataBound
+                    .exit()
+                    .remove();
+                var enterSelection = dataBound
+                    .enter()
+                    .append('g')
+                    .classed('data', true)
+                    .attr('transform', d => `translate(${xScale(+d.key)},${yScale(d.values.length)})`);
+                enterSelection.append('circle')
+                    .attr({
+                        'cx': 0,
+                        'cy': 0,
+                        'r': 2
+                    });
             }
         });
 } ());
