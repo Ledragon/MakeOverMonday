91c39e900208cf8863e1e7653e0574c5bb031274
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-06-26 18:53:22 +0200

    Data reading and conversion

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 (function () {
     'use strict';
-    d3.select('#container').append('rect')
-        .attr({
-            'x': 0,
-            'y': 0,
-            'width': 100,
-            'height': 100
-        })
-        .style('fill', 'lightblue');
+
+    d3.csv('data/Makeover Monday.csv',
+        (d) => {
+            return {
+                pinterestBoard: d['Pinterest Board'],
+                pinboardUrl: d['Pinboard URL'],
+                description: d.Description,
+                pinNote: d['Pin Note'],
+                originalLink: d['Original Link'],
+                name: d['Name'],
+                week: +d.Week,
+                datePinned: d['Date Pinned']
+            }
+        },
+        (error, data) => {
+            if (error) {
+                console.error(error);
+            } else {
+                console.log(data);
+                d3.nest()
+                    .key(d => d.Week);
+            }
+        });
 } ());
