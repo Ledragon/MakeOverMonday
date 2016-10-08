9e328a618d667932fcde0ba8d7dd7c9b6e474456
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-10-08 22:17:37 +0200

    Complete

--- src/app.js
+++ src/app.js
@@ -8,59 +8,89 @@ var d3_geo_1 = require('d3-geo');
 var d3_scale_1 = require('d3-scale');
 var d3_scale_chromatic_1 = require('d3-scale-chromatic');
 var node = d3_selection_1.select('.content').node().getBoundingClientRect();
-var width = node.width; //- 20;
-var height = node.height; // - 10;
+var width = 960; //node.width;//- 20;
+var height = 640; //node.height;// - 10;
 var chartSvg = d3_selection_1.select('.content')
     .append('svg')
     .attr('width', width)
     .attr('height', height);
-var colorScale = d3_scale_1.scaleLinear()
-    .range([0, 1]);
-var q = d3_queue_1.queue()
-    .defer(d3_request_1.csv, 'data/Global Peace Index 2016.csv')
-    .defer(d3_request_1.json, 'data/countries.json')
-    .await(function (error, csvData, world) {
-    if (error) {
-        console.error(error);
-    }
-    else {
-        var byYear = d3_collection_1.nest()
-            .key(function (d) { return d.Year; })
-            .entries(csvData);
-        var years = byYear.map(function (d) { return +d.key; });
-        var projection = d3_geo_1.geoMercator()
-            .fitSize([width, height], world);
-        var pathGenerator_1 = d3_geo_1.geoPath()
-            .projection(projection);
-        // console.log(pathGenerator(world.features[0]));
-        chartSvg
-            .selectAll('path')
-            .data(world.features)
-            .enter()
-            .append('path')
-            .classed('country', true)
-            .attr('d', function (d) { return pathGenerator_1(d); });
-        var first = byYear[0].values;
+var legend = chartSvg.append('g')
+    .classed('legend', true)
+    .attr('transform', "translate(" + 30 + "," + 50 + ")");
+legend.append('text')
+    .style('font-size', '36px')
+    .text(2000);
+var scaleLegend = legend.append('g')
+    .classed('scale', true)
+    .attr('transform', "translate(" + 0 + "," + 10 + ")");
+var rectWidth = 2;
+scaleLegend.selectAll('rect')
+    .data(d3_array_1.range(0, 1, 0.01))
+    .enter()
+    .append('rect')
+    .attr('x', function (d, i) { return i * rectWidth; })
+    .attr('height', 10)
+    .attr('width', rectWidth)
+    .style('fill', function (d) { return d3_scale_chromatic_1.interpolateBlues(d); });
+legend.append('text')
+    .text('Less peaceful')
+    .attr('transform', "translate(" + 0 + "," + 30 + ")");
+legend.append('text')
+    .text('More peaceful')
+    .style('text-anchor', 'end')
+    .attr('transform', "translate(" + 200 + "," + 30 + ")");
+var projection = d3_geo_1.geoMercator();
+// .fitSize([width, height], world);
+var pathGenerator = d3_geo_1.geoPath()
+    .projection(projection);
+drawMap();
+function drawMap() {
+    var colorScale = d3_scale_1.scaleLinear()
+        .range([0, 1]);
+    var q = d3_queue_1.queue()
+        .defer(d3_request_1.csv, 'data/Global Peace Index 2016.csv')
+        .defer(d3_request_1.json, 'data/countries.json')
+        .await(function (error, csvData, world) {
+        if (error) {
+            console.error(error);
+        }
+        else {
+            var byYear_1 = d3_collection_1.nest()
+                .key(function (d) { return d.Year; })
+                .entries(csvData);
+            var years = byYear_1.map(function (d) { return +d.key; });
+            // console.log(pathGenerator(world.features[0]));
+            projection.fitSize([width, height], world);
+            chartSvg
+                .selectAll('path')
+                .data(world.features)
+                .enter()
+                .append('path')
+                .classed('country', true)
+                .attr('d', function (d) { return pathGenerator(d); });
+            var i_1 = 0;
+            setInterval(function () {
+                if (i_1 >= byYear_1.length) {
+                    i_1 = 0;
+                }
+                updateMap(byYear_1[i_1]);
+                i_1++;
+            }, 500);
+        }
+    });
+    function updateMap(kvp) {
+        chartSvg.select('text').text(kvp.key);
+        var first = kvp.values;
         colorScale.domain(d3_array_1.extent(first, function (f) { return parseFloat(f.Score); }));
-        console.log(colorScale.domain());
         chartSvg.selectAll('path')
             .style('fill', function (d) {
             var index = first.filter(function (f) { return f.Code === d.properties.ADM0_A3; });
             if (index.length > 0) {
                 var found = index[0];
-                console.log(found);
+                // console.log(found)
                 return d3_scale_chromatic_1.interpolateBlues(colorScale(parseFloat(found.Score)));
             }
         });
     }
-});
-// q.defer(csv('data/Global Peace Index 2016.csv'))
-// csv('data/Global Peace Index 2016.csv', function (error: any, data: any) {
-//     if (error) {
-//         console.error(error);
-//     }
-//     else {
-//         console.log(data);
-//     }
-// }) 
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,68 +2,103 @@ import { csv, json, request } from 'd3-request';
 import { select } from 'd3-selection';
 import { queue } from 'd3-queue';
 import { nest } from 'd3-collection';
-import { max, extent } from 'd3-array';
+import { max, extent, range } from 'd3-array';
 import { geoMercator, geoPath } from 'd3-geo';
 import { scaleLinear } from 'd3-scale';
 import { interpolateBlues } from 'd3-scale-chromatic';
 
 
 let node = (select('.content').node() as any).getBoundingClientRect();
-let width = node.width;//- 20;
-let height = node.height;// - 10;
+let width = 960//node.width;//- 20;
+let height = 640//node.height;// - 10;
 let chartSvg = select('.content')
     .append('svg')
     .attr('width', width)
     .attr('height', height);
-let colorScale = scaleLinear()
-    .range([0, 1]);
-var q = queue()
-    .defer(csv, 'data/Global Peace Index 2016.csv')
-    .defer(json, 'data/countries.json')
-    .await((error, csvData, world) => {
-        if (error) {
-            console.error(error);
-        } else {
-            let byYear = nest()
-                .key(d => d.Year)
-                .entries(csvData);
-            let years = byYear.map(d => +d.key);
 
-            let projection = geoMercator()
-                .fitSize([width, height], world);
-            let pathGenerator = geoPath()
-                .projection(projection);
-            // console.log(pathGenerator(world.features[0]));
-            chartSvg
-                .selectAll('path')
-                .data(world.features)
-                .enter()
-                .append('path')
-                .classed('country', true)
-                .attr('d', d => pathGenerator(d));
+let legend = chartSvg.append('g')
+    .classed('legend', true)
+    .attr('transform', `translate(${30},${50})`)
+legend.append('text')
+    .style('font-size', '36px')
+    .text(2000);
 
-            var first = byYear[0].values;
-            colorScale.domain(extent(first, f => parseFloat(f.Score)));
-            console.log(colorScale.domain())
-            chartSvg.selectAll('path')
-                .style('fill', d => {
-                    let index = first.filter(f => f.Code === d.properties.ADM0_A3);
-                    if (index.length > 0) {
-                        let found = index[0];
-                        console.log(found)
-                        return interpolateBlues(colorScale(parseFloat(found.Score)));
+let scaleLegend = legend.append('g')
+    .classed('scale', true)
+    .attr('transform', `translate(${0},${10})`);
+let rectWidth = 2;
+scaleLegend.selectAll('rect')
+    .data(range(0, 1, 0.01))
+    .enter()
+    .append('rect')
+    .attr('x', (d, i) => i * rectWidth)
+    .attr('height', 10)
+    .attr('width', rectWidth)
+    .style('fill', d => interpolateBlues(d));
+legend.append('text')
+    .text('Less peaceful')
+    .attr('transform', `translate(${0},${30})`)
+legend.append('text')
+    .text('More peaceful')
+    .style('text-anchor', 'end')
+    .attr('transform', `translate(${200},${30})`)
+
+
+let projection = geoMercator()
+// .fitSize([width, height], world);
+let pathGenerator = geoPath()
+    .projection(projection);
+
+drawMap();
+
+function drawMap() {
+
+    let colorScale = scaleLinear()
+        .range([0, 1]);
+    var q = queue()
+        .defer(csv, 'data/Global Peace Index 2016.csv')
+        .defer(json, 'data/countries.json')
+        .await((error, csvData, world) => {
+            if (error) {
+                console.error(error);
+            } else {
+                let byYear = nest()
+                    .key(d => d.Year)
+                    .entries(csvData);
+                let years = byYear.map(d => +d.key);
+                // console.log(pathGenerator(world.features[0]));
+                projection.fitSize([width, height], world);
+                chartSvg
+                    .selectAll('path')
+                    .data(world.features)
+                    .enter()
+                    .append('path')
+                    .classed('country', true)
+                    .attr('d', d => pathGenerator(d));
+
+                let i = 0;
+                setInterval(() => {
+                    if (i >= byYear.length) {
+                        i = 0;
                     }
-                })
+                    updateMap(byYear[i]);
+                    i++;
+                }, 500);
+            }
+        });
 
-        }
-    });
-
-// q.defer(csv('data/Global Peace Index 2016.csv'))
-// csv('data/Global Peace Index 2016.csv', function (error: any, data: any) {
-//     if (error) {
-//         console.error(error);
-//     }
-//     else {
-//         console.log(data);
-//     }
-// })
+    function updateMap(kvp: { key: string, values: Array<any> }) {
+        chartSvg.select('text').text(kvp.key);
+        var first = kvp.values;
+        colorScale.domain(extent(first, f => parseFloat(f.Score)));
+        chartSvg.selectAll('path')
+            .style('fill', d => {
+                let index = first.filter(f => f.Code === d.properties.ADM0_A3);
+                if (index.length > 0) {
+                    let found = index[0];
+                    // console.log(found)
+                    return interpolateBlues(colorScale(parseFloat(found.Score)));
+                }
+            });
+    }
+}
