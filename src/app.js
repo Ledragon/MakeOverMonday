ea743bca88c4b91c26d1cd48363133f75c90a05d
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-10-17 19:39:43 +0200

    Initial chart

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 "use strict";
+var d3_selection_1 = require('d3-selection');
 var d3_request_1 = require('d3-request');
 var d3_collection_1 = require('d3-collection');
 var d3_array_1 = require('d3-array');
+var d3_scale_1 = require('d3-scale');
+var svg1 = d3_selection_1.select('#chart')
+    .append('svg')
+    .attr('width', 900)
+    .attr('height', 780);
 d3_request_1.csv('data/data.csv', function (error, data) {
     if (error) {
         console.error(error);
@@ -16,16 +22,76 @@ d3_request_1.csv('data/data.csv', function (error, data) {
             .entries(bySurveyDate[0].values);
         console.log(byCountry2012);
         var mapped = byCountry2012.map(function (d) {
+            var total = d3_array_1.sum(d.values, function (v) { return parseFloat(v.TOTAL); });
             return {
                 country: d.key,
-                total: d3_array_1.sum(d.values, function (v) { return parseFloat(v.TOTAL); }),
-                verySatisfied: d3_array_1.sum(d.values, function (v) { return parseFloat(v['Very satisfied']); }),
-                ratherSatisfied: d3_array_1.sum(d.values, function (v) { return parseFloat(v['Rather satisfied']); }),
-                ratherUnsatisfied: d3_array_1.sum(d.values, function (v) { return parseFloat(v['Rather unsatisfied']); }),
-                notAtAllSatisfied: d3_array_1.sum(d.values, function (v) { return parseFloat(v['Not at all satisfied']); }),
-                dontKnow: d3_array_1.sum(d.values, function (v) { return parseFloat(v['Don\'t know']); }),
+                verySatisfied: d3_array_1.sum(d.values, function (v) { return parseFloat(v['Very satisfied']); }) / total,
+                ratherSatisfied: d3_array_1.sum(d.values, function (v) { return parseFloat(v['Rather satisfied']); }) / total,
+                ratherUnsatisfied: d3_array_1.sum(d.values, function (v) { return parseFloat(v['Rather unsatisfied']); }) / total,
+                notAtAllSatisfied: d3_array_1.sum(d.values, function (v) { return parseFloat(v['Not at all satisfied']); }) / total,
+                dontKnow: d3_array_1.sum(d.values, function (v) { return parseFloat(v['Don\'t know']); }) / total,
             };
         });
+        console.log(mapped);
+        var c1 = svg1.append('g')
+            .classed('c2012', true)
+            .attr('transform', "translate(" + 0 + "," + 0 + ")");
+        // c1.append('text')
+        //     .style('text-anchor', 'middle')
+        //     .attr('x', 200)
+        //     .attr('y', 0)
+        //     .text('Total')
+        // c1.append('text')
+        //     .style('text-anchor', 'middle')
+        //     .attr('x', 300)
+        //     .attr('y', 0)
+        //     .text('Very')
+        var scale_1 = d3_scale_1.scaleLinear()
+            .range([0, 600])
+            .domain([0, 1]);
+        var enterSelection = c1.selectAll('.country')
+            .data(mapped)
+            .enter()
+            .append('g')
+            .classed('country', true)
+            .attr('transform', function (d, i) { return ("translate(" + 0 + "," + (i * 22 + 10) + ")"); });
+        enterSelection.append('text')
+            .attr('x', 10)
+            .attr('y', 15)
+            .text(function (d, i) { return i + '. ' + d.country; });
+        var rects = enterSelection.append('g')
+            .classed('rects', true)
+            .attr('transform', "translate(" + 200 + "," + 0 + ")");
+        rects.append('rect')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('height', 20)
+            .style('fill', d3_scale_1.schemeCategory10[3])
+            .attr('width', function (d) { return scale_1(d.notAtAllSatisfied); });
+        rects.append('rect')
+            .attr('x', function (d) { return scale_1(d.notAtAllSatisfied); })
+            .attr('y', 0)
+            .attr('height', 20)
+            .style('fill', d3_scale_1.schemeCategory10[1])
+            .attr('width', function (d) { return scale_1(d.ratherUnsatisfied); });
+        rects.append('rect')
+            .attr('x', function (d) { return scale_1(d.notAtAllSatisfied + d.ratherUnsatisfied); })
+            .attr('y', 0)
+            .attr('height', 20)
+            .style('fill', d3_scale_1.schemeCategory10[8])
+            .attr('width', function (d) { return scale_1(d.ratherSatisfied); });
+        rects.append('rect')
+            .attr('x', function (d) { return scale_1(d.notAtAllSatisfied + d.ratherUnsatisfied + d.ratherSatisfied); })
+            .attr('y', 0)
+            .attr('height', 20)
+            .style('fill', d3_scale_1.schemeCategory10[2])
+            .attr('width', function (d) { return scale_1(d.verySatisfied); });
+        rects.append('rect')
+            .attr('x', function (d) { return scale_1(d.ratherSatisfied + d.verySatisfied + d.ratherUnsatisfied + d.notAtAllSatisfied); })
+            .attr('y', 0)
+            .attr('height', 20)
+            .style('fill', d3_scale_1.schemeCategory10[0])
+            .attr('width', function (d) { return scale_1(d.dontKnow); });
     }
 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
+import { select } from 'd3-selection';
 import { csv } from 'd3-request';
 import { nest } from 'd3-collection';
-import { sum } from 'd3-array';
+import { sum, max } from 'd3-array';
 import { scaleLinear, schemeCategory10 } from 'd3-scale';
 import { line } from 'd3-shape';
+
+let svg1 = select('#chart')
+    .append('svg')
+    .attr('width', 900)
+    .attr('height', 780)
 
 csv('data/data.csv', (error, data) => {
     if (error) {
@@ -12,24 +18,98 @@ csv('data/data.csv', (error, data) => {
         let bySurveyDate = nest()
             .key(d => d['Survey Date'])
             .entries(data);
-        
+
 
         let byCountry2012 = nest()
             .key(d => d.Country)
             .entries(bySurveyDate[0].values);
-        
+
 
         console.log(byCountry2012)
         let mapped = byCountry2012.map(d => {
+            let total = sum(d.values, v => parseFloat(v.TOTAL));
             return {
                 country: d.key,
-                total: sum(d.values, v => parseFloat(v.TOTAL)),
-                verySatisfied: sum(d.values, v => parseFloat(v['Very satisfied'])),
-                ratherSatisfied: sum(d.values, v => parseFloat(v['Rather satisfied'])),
-                ratherUnsatisfied: sum(d.values, v => parseFloat(v['Rather unsatisfied'])),
-                notAtAllSatisfied: sum(d.values, v => parseFloat(v['Not at all satisfied'])),
-                dontKnow: sum(d.values, v => parseFloat(v['Don\'t know'])),
+                verySatisfied: sum(d.values, v => parseFloat(v['Very satisfied']))/total,
+                ratherSatisfied: sum(d.values, v => parseFloat(v['Rather satisfied']))/total,
+                ratherUnsatisfied: sum(d.values, v => parseFloat(v['Rather unsatisfied']))/total,
+                notAtAllSatisfied: sum(d.values, v => parseFloat(v['Not at all satisfied']))/total,
+                dontKnow: sum(d.values, v => parseFloat(v['Don\'t know']))/total,
             };
         });
+        console.log(mapped);
+        let c1 = svg1.append('g')
+            .classed('c2012', true)
+            .attr('transform', `translate(${0},${0})`);
+        // c1.append('text')
+        //     .style('text-anchor', 'middle')
+        //     .attr('x', 200)
+        //     .attr('y', 0)
+        //     .text('Total')
+        // c1.append('text')
+        //     .style('text-anchor', 'middle')
+        //     .attr('x', 300)
+        //     .attr('y', 0)
+        //     .text('Very')
+        let scale = scaleLinear()
+            .range([0, 600])
+            .domain([0, 1]);
+        var enterSelection = c1.selectAll('.country')
+            .data(mapped)
+            .enter()
+            .append('g')
+            .classed('country', true)
+            .attr('transform', (d, i) => `translate(${0},${i * 22 + 10})`);
+
+        enterSelection.append('text')
+            .attr('x', 10)
+            .attr('y', 15)
+            .text((d, i) => i + '. ' + d.country)
+        let rects = enterSelection.append('g')
+            .classed('rects', true)
+            .attr('transform', `translate(${200},${0})`);
+        
+        rects.append('rect')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('height', 20)
+            .style('fill', schemeCategory10[3])
+            .attr('width', d => scale(d.notAtAllSatisfied))
+        
+        rects.append('rect')
+            .attr('x', d => scale(d.notAtAllSatisfied))
+            .attr('y', 0)
+            .attr('height', 20)
+            .style('fill', schemeCategory10[1])
+            .attr('width', d => scale(d.ratherUnsatisfied))
+        
+        rects.append('rect')
+            .attr('x', d => scale(d.notAtAllSatisfied+d.ratherUnsatisfied))
+            .attr('y', 0)
+            .attr('height', 20)
+            .style('fill', schemeCategory10[8])
+            .attr('width', d => scale(d.ratherSatisfied))
+        rects.append('rect')
+            .attr('x', d => scale(d.notAtAllSatisfied+d.ratherUnsatisfied+d.ratherSatisfied))
+            .attr('y', 0)
+            .attr('height', 20)
+            .style('fill', schemeCategory10[2])
+            .attr('width', d => scale(d.verySatisfied))
+        rects.append('rect')
+            .attr('x', d => scale(d.ratherSatisfied+d.verySatisfied+d.ratherUnsatisfied+d.notAtAllSatisfied))
+            .attr('y', 0)
+            .attr('height', 20)
+            .style('fill', schemeCategory10[0])
+            .attr('width', d => scale(d.dontKnow))
+        // enterSelection.append('text')
+        //     .style('text-anchor', 'middle')
+        //     .attr('x', 200)
+        //     .attr('y', 15)
+        //     .text((d, i) => d.total)
+        // enterSelection.append('text')
+        //     .style('text-anchor', 'middle')
+        //     .attr('x', 300)
+        //     .attr('y', 15)
+        //     .text((d, i) => d.verySatisfied)
     }
 });
