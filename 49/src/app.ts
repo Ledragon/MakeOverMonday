3d0b306327e87003cfc04e01cc956ca248d1732b
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-12-17 16:36:12 +0100

    Chord diagram

--- 49/src/app.ts
+++ 49/src/app.ts
@@ -1,6 +1,6 @@
 import * as d3 from 'd3';
-let width = 960;
-let height = 480;
+let width = 800;
+let height = 800;
 
 let svg = d3.select('#chart')
     .append('svg')
@@ -10,99 +10,80 @@ let svg = d3.select('#chart')
 let plotMargins = {
     top: 30,
     bottom: 30,
-    left: 50,
+    left: 30,
     right: 30
 };
 let plotGroup = svg.append('g')
     .classed('plot', true)
     .attr('transform', `translate(${plotMargins.left},${plotMargins.top})`);
-
+let colors = d3.schemeCategory10;
 let plotWidth = width - plotMargins.left - plotMargins.right;
 let plotHeight = height - plotMargins.top - plotMargins.bottom;
 
+let chordGenerator = d3.chord()
+    .padAngle(0.05);
+let outerRadius = (plotWidth - plotMargins.left) / 2;
+let arcGenerator = d3.arc()
+    .outerRadius(outerRadius)
+    .innerRadius(outerRadius - 10);
+let ribbonGenerator = d3.ribbon()
+    .radius(outerRadius - 10);
 
-let xScale = d3.scaleLinear()
-    .domain([0, 1])
-    .range([0, plotWidth])
-let xAxis = d3.axisBottom(xScale)
-    .tickFormat(d3.format('0.0f'));
+let container = plotGroup.append('g')
+    .attr('transform', `translate(${plotWidth / 2},${plotHeight / 2})`)
 
-let xAxisGroup = plotGroup.append('g')
-    .classed('axis', true)
-    .attr('transform', `translate(${0},${plotHeight})`)
-    .call(xAxis);
-
-let yScale = d3.scaleLinear()
-    .domain([0, 1])
-    .range([plotHeight, 0]);
-let yAxis = d3.axisLeft(yScale)
-    .tickFormat(d3.format('2.0%'));
-let yAxisGroup = plotGroup.append('g')
-    .classed('axis', true)
-    .attr('transform', `translate(${0},${0})`)
-    .call(yAxis);
-
-
-// let lineGeneratorBottom90 = d3.line<any>()
-//     .x(d => xScale(d.year))
-//     .y(d => yScale(d.bottom90));
-// let lineGeneratorTop10 = d3.line<any>()
-//     .x(d => xScale(d.year))
-//     .y(d => yScale(d.top10));
-let areaGeneratorBottom90 = d3.area<any>()
-    .x(d => xScale(d.data.year))
-    .y0((d, i) => {
-        let y0 = isNaN(d[0]) ? yScale(0) : yScale(d[0]);
-        return y0;
-    })
-    .y1((d, i) => {
-        let y1: number = isNaN(d[1]) ? yScale(d[0]) : yScale(d[1]);
-        return y1;
-    });
-
-d3.csv('data/Inequality.csv', (d: any) => {
-    let par = (dd: string) => { return dd ? parseFloat(dd.replace('%', '')) / 100 : null; };
+d3.csv('data/data.csv', (d: any) => {
     return {
-        year: parseInt(d.Year),
-        bottom90: par(d['Bottom 90%']),
-        top10: par(d['Top 10%'])
+        origin: d['Origin'],
+        destination: d['Destination'],
+        '1990': parseFloat(d['1990']),
+        '1995': parseFloat(d['1995']),
+        '2000': parseFloat(d['2000']),
+        '2005': parseFloat(d['2005']),
     };
 }, (error: any, data: Array<any>) => {
     if (error) {
         console.error(error);
     } else {
-        data = data.filter(d => !!d.bottom90);
-
-
-        let stackGenerator = d3.stack<any>()
-            .keys(['top10', 'bottom90']);
-        let stacked = stackGenerator(data);
-        console.log(stacked);
         // console.log(data);
-        xScale.domain(d3.extent(data, d => d.year));
-        xAxisGroup.call(xAxis);
-        var dataBound = plotGroup.selectAll('.series')
-            .data(stacked);
-        dataBound
-            .exit()
-            .remove();
-        let enterSelection = dataBound
+        let byOriign = d3.nest<any>()
+            .key(d => d.origin)
+            .entries(data);
+        // console.log(byOriign);
+        let matrix = byOriign.map(d => d.values.map(dd => dd['2005']));
+        // console.log(matrix);
+        let chords = chordGenerator(matrix);
+        let g = container.datum(chords);
+        let group = g.append('g')
+            .classed('groups', true)
+            .selectAll('g')
+            .data(d => d.groups)
             .enter()
-            .append('g')
-            .attr('class', d => d.key)
-            .classed('series', true);
-        enterSelection.append('path')
-            .attr('d', areaGeneratorBottom90);
-        enterSelection.append('text')
-            .attr('transform', (d,i)=>`translate(${plotWidth / 2},${yScale(1-d3.mean(data, dd=>dd[d.key]))})`)
-            .text(d => d.key);
-        // plotGroup.append('path')
-        //     .classed('bottom-90', true)
-        //     // .data(data)
-        //     .attr('d', lineGeneratorBottom90(data));
-        // plotGroup.append('path')
-        //     .classed('top-10', true)
-        //     // .data(data)
-        //     .attr('d', lineGeneratorTop10(data));
+            .append('g');
+
+        group.append('path')
+            .attr('d', arcGenerator)
+            .style('fill', (d, i) => colors[i])
+
+        g.append("g")
+            .attr("class", "ribbons")
+            .selectAll("path")
+            .data(function (chords) { return chords; })
+            .enter().append("path")
+            .attr("d", ribbonGenerator)
+            .style("fill", (d) => { return colors[d.target.index]; })
+        // .style("stroke", function(d) { return d3.rgb(color(d.target.index)).darker(); });
+        // var dataBound = container.selectAll('.chord')
+        //     .data(chords);
+        // dataBound
+        //   .exit()
+        //   .remove();
+        // let enterSelection = dataBound
+        //   .enter()
+        //   .append('g')
+        //     .classed('chord', true);
+        // enterSelection.append('path')
+        // .attr('d', arcGenerator)
+
     }
 });
