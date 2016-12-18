636785c2b7f020af9d52b52644f86a174b05bf46
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-12-18 16:26:52 +0100

    MOM 50

--- 50/src/app.ts
+++ 50/src/app.ts
@@ -1,6 +1,8 @@
 import * as d3 from 'd3';
 let width = 800;
-let height = 800;
+let height = 830;
+let green = '#84B082';
+let red = '#885A5A';
 
 let svg = d3.select('#chart')
     .append('svg')
@@ -14,9 +16,9 @@ svg.append('marker')
     .append('path')
     .attr('d', 'M0,0 V4 L2, 2Z');
 let plotMargins = {
-    top: 30,
+    top: 50,
     bottom: 30,
-    left: 30,
+    left: 120,
     right: 30
 };
 let plotGroup = svg.append('g')
@@ -42,5 +44,30 @@ d3.csv('data/data.csv', (d: any) => {
         console.error(error);
     } else {
         var sorted = data.sort((a, b) => a.rank - b.rank);
+        let statesScale = d3.scaleBand()
+            .domain(sorted.map(d => d.state))
+            .range([0, plotHeight])
+            .padding(.3);
+        let statesAxis = d3.axisLeft(statesScale);
+        let statesAxisGroup = plotGroup.append('g')
+            .classed('axis', true)
+            .call(statesAxis);
+        let scoreScale = d3.scaleLinear()
+            .domain([0, d3.max(data, d => d.totalScore)])
+            .range([0, plotWidth]);
+        let colorScale = d3.scaleLinear<any>()
+            .domain(d3.extent(data, d => d.totalScore))
+            .range([d3.rgb(green), d3.rgb(red)])
+            .interpolate(d3.interpolateHcl);
+        plotGroup.selectAll('g.score')
+            .data(sorted)
+            .enter()
+            .append('g')
+            .classed('score', true)
+            .attr('transform', d => `translate(${0},${statesScale(d.state)})`)
+            .append('rect')
+            .attr('width', d=>scoreScale(d.totalScore))
+            .attr('height', statesScale.bandwidth())
+        .style('fill', d=>colorScale(d.totalScore))
     }
 });
