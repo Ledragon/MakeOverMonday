a5e887964d3659b77c20f5309a5e61b211f96fbf
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-12-19 11:52:30 +0100

    Lines working

--- 51/src/app.ts
+++ 51/src/app.ts
@@ -1,6 +1,6 @@
 import * as d3 from 'd3';
-let width = 800;
-let height = 830;
+let width = 960;
+let height = 480;
 let green = '#84B082';
 let red = '#885A5A';
 
@@ -10,9 +10,9 @@ let svg = d3.select('#chart')
     .attr('height', height);
 
 let plotMargins = {
-    top: 50,
+    top: 30,
     bottom: 30,
-    left: 120,
+    left: 60,
     right: 30
 };
 let plotGroup = svg.append('g')
@@ -22,21 +22,97 @@ let plotGroup = svg.append('g')
 let plotWidth = width - plotMargins.left - plotMargins.right;
 let plotHeight = height - plotMargins.top - plotMargins.bottom;
 
-d3.csv('data/data.csv', (error: any, data: any) => {
+let yScale = d3.scaleLinear()
+    .domain([0, 1])
+    .range([plotHeight, 0]);
+let yAxis = d3.axisLeft(yScale)
+    // .tickFormat(d3.format('2.0%'));
+let yAxisGroup = plotGroup.append('g')
+    .classed('axis', true)
+    .call(yAxis);
+
+const dateFormat = '%Y-%b';
+let xScale = d3.scaleTime()
+    // .domain([new Da])
+    .range([0, plotWidth])
+let xAxis = d3.axisBottom(xScale)
+    .tickFormat(d3.timeFormat('%b %y'));
+let xAxisGroup = plotGroup.append('g')
+    .classed('axis', true)
+    .attr('transform', `translate(${0},${plotHeight})`)
+    .call(xAxis);
+plotGroup.append('g')
+    .classed('series', true)
+    .append('path');
+plotGroup.append('g')
+    .classed('target', true)
+    .append('path');
+let lineGenerator = d3.line<any>()
+    .x(d => xScale(d.date));
+let targetGenerator = d3.line<any>()
+    .x(d => xScale(d.date));
+
+d3.csv('data/data.csv', (error: any, data: IDataFormat) => {
     if (error) {
         console.error(error);
     } else {
         console.log(data);
         let categories = data.columns.splice(2, 20).filter(d => d.indexOf('Target') < 0);
         console.log(categories);
-
-
+        let filtered = data
+            .filter(d => d.Month.indexOf('YTD') < 0)
+            .map(d => {
+                let obj = {
+                    year: d['Year'].replace('CY ', ''),
+                    month: d.Month
+                }
+                let date = d3.timeParse(dateFormat)(obj.year + '-' + obj.month);
+                let res: any = {
+                    date: date
+                };
+                for (var key in d) {
+                    if (key !== 'Year' && key !== 'Month') {
+                        res[key] = parseFloat(d[key])
+                    };
+                }
+                return res;
+            })
+            .sort((a, b) => a.date - b.date);
+        let dates = filtered.map(d => d.date);
+        console.log(dates);
+        xScale.domain(d3.extent(dates));
+        xAxisGroup.call(xAxis);
         d3.select('#menu')
             .selectAll('.menu-item')
             .data(categories)
             .enter()
             .append('div')
             .classed('menu-item', true)
+            .on('click', function (d) {
+                d3.selectAll('.menu-item')
+                    .classed('highlight', false);
+                d3.select(this)
+                    .classed('highlight', true);
+                updateLines(filtered,d);
+            })
             .text(d => d)
     }
 });
+
+function updateLines(data: Array<any>, propertyName: string) {
+    yScale.domain(d3.extent(data, d => d[propertyName])).nice();
+    yAxisGroup.call(yAxis);
+    lineGenerator.y(d => yScale(d[propertyName]));
+    targetGenerator.y(d => yScale(d[propertyName + ' Target']));
+    plotGroup.select('.series')
+        .select('path')
+        .attr('d', lineGenerator(data.filter(d=>!!d[propertyName])))
+    plotGroup.select('.target')
+        .select('path')
+        .attr('d', targetGenerator(data.filter(d=>!!d[propertyName])))
+}
+
+
+interface IDataFormat extends Array<any> {
+    columns: string[];
+}
