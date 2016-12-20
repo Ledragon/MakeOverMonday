8f65ab72ffb9572d5a80b439b9708df4f9fa496d
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-12-20 16:16:47 +0100

    51 refactored

---
+++ src/mom/50/state.ts
@@ -0,0 +1,10 @@
+import * as angular from 'angular';
+
+export var mom50State = {
+    name: 'mom50',
+    template: '<mom50 class=\"flex-container\"></mom50>',
+    url: '/mom50',
+    data: {
+        description: 'Which States have the worst drivers?'
+    }
+};
--- src/mom/momModule.ts
+++ src/mom/momModule.ts
@@ -1,11 +1,15 @@
 import * as angular from 'angular';
 
+import { mom50 } from './50/component';
 import { mom51 } from './51/component';
+import { mom50State } from './50/state';
 import { mom51State } from './51/state';
 
 export const momName = angular.module('momModule', [])
     .config(($stateProvider: angular.ui.IStateProvider) => {
+        $stateProvider.state(mom50State);
         $stateProvider.state(mom51State);
     })    
+    .component(mom50.name, mom50.component)
     .component(mom51.name, mom51.component)
     .name;
