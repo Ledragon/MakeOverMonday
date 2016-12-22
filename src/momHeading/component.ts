eb1dd8a7c7e304a2093cd994e71f52ead81e9f28
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-12-22 15:10:06 +0100

    Heading fix

--- src/momHeading/component.ts
+++ src/momHeading/component.ts
@@ -1,8 +1,10 @@
 export class momHeadingController {
     name: string = 'test';
     description: string = 'desc';
-    static $inject = ['$scope'];
-    constructor($scope: angular.IScope) {
+    static $inject = ['$scope', '$state'];
+    constructor($scope: angular.IScope, $state: angular.ui.IStateService) {
+        this.name = $state.current.name.replace('mom', '');
+        this.description = $state.current.data.description;
         $scope.$on('$stateChangeSuccess', (event, toState: angular.ui.IState) => {
             this.name = toState.name.replace('mom', '');
             this.description = toState.data.description;
