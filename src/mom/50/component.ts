3b0a7d529604d0b1fd0df5d8c8b0fb7b65f1b794
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-12-29 18:23:21 +0100

    Refactoring 50

--- src/mom/50/component.ts
+++ src/mom/50/component.ts
@@ -1,5 +1,6 @@
 import * as d3 from 'd3';
 import * as plot from '../../charting/plotFactory';
+import { LeftCategoricalAxis } from '../../charting/LeftCategorical';
 
 export var mom50 = {
     name: 'mom50',
@@ -12,7 +13,7 @@ export var mom50 = {
             let red = '#885A5A';
 
             let plotMargins = {
-                top: 50,
+                top: 30,
                 bottom: 30,
                 left: 120,
                 right: 30
@@ -22,38 +23,36 @@ export var mom50 = {
             let plotGroup = p.group();
             let plotHeight = p.height();
             let plotWidth = p.width();
-
+            let yAxis = new LeftCategoricalAxis(plotGroup, plotWidth, plotHeight)
+                .padding(.3);
             csvService.read('mom/50/data/data.csv',
                 (data: Array<any>) => {
-                    var sorted = data.sort((a, b) => a.rank - b.rank);
-                    let statesScale = d3.scaleBand()
-                        .domain(sorted.map(d => d.state))
-                        .range([0, plotHeight])
-                        .padding(.3);
-                    let statesAxis = d3.axisLeft(statesScale);
-                    let statesAxisGroup = plotGroup.append('g')
-                        .classed('axis', true)
-                        .call(statesAxis);
-                    let scoreScale = d3.scaleLinear()
-                        .domain([0, d3.max(data, d => d.totalScore)])
-                        .range([0, plotWidth]);
-                    let colorScale = d3.scaleLinear<any>()
-                        .domain(d3.extent(data, d => d.totalScore))
-                        .range([d3.rgb(green), d3.rgb(red)])
-                        .interpolate(d3.interpolateHcl);
-                    plotGroup.selectAll('g.score')
-                        .data(sorted)
-                        .enter()
-                        .append('g')
-                        .classed('score', true)
-                        .attr('transform', d => `translate(${0},${statesScale(d.state)})`)
-                        .append('rect')
-                        .attr('width', d => scoreScale(d.totalScore))
-                        .attr('height', statesScale.bandwidth())
-                        .style('fill', d => colorScale(d.totalScore))
+                    update(<any>data);
                 },
                 parseFunction);
 
+            function update(data: IDataFormat) {
+                var sorted = data.sort((a, b) => a.rank - b.rank);
+                yAxis.domain(sorted.map(d => d.state));
+
+                let scoreScale = d3.scaleLinear()
+                    .domain([0, d3.max(data, d => d.totalScore)])
+                    .range([0, plotWidth]);
+                let colorScale = d3.scaleLinear<any>()
+                    .domain(d3.extent(data, d => d.totalScore))
+                    .range([d3.rgb(green), d3.rgb(red)])
+                    .interpolate(d3.interpolateHcl);
+                plotGroup.selectAll('g.score')
+                    .data(sorted)
+                    .enter()
+                    .append('g')
+                    .classed('score', true)
+                    .attr('transform', d => `translate(${0},${yAxis.scale(d.state)})`)
+                    .append('rect')
+                    .attr('width', d => scoreScale(d.totalScore))
+                    .attr('height', yAxis.bandWidth())
+                    .style('fill', d => colorScale(d.totalScore))
+            }
 
             interface IDataFormat extends Array<any> {
                 columns: string[];
